2bb1aa8ae07348f89bf8743eda3aa0c202df27f5
agusoler000 <agustin.soler@mcassociats.com>
2022-05-16 15:06:43 +0200

    chore: diferent method

--- parser.py
+++ parser.py
@@ -1,37 +1,49 @@
 from PyPDF2 import PdfFileReader
 from pathlib import Path
+import re
+import openpyxl
 
 pdf = PdfFileReader('111JUL19-1.pdf')
+excel = openpyxl.load_workbook('excelprueba.xlsx')
+sheet1 = excel['Sheet1']
 
 
 
 #Extract The page
 page0Object = pdf.getPage(0)
-# print(page0Object)
-
+#Get Count
+# page_count = page0Object.getNumPages()
 #Extract the Text
 page1text = page0Object.extractText()
 # print(page1text)
 
 
+
 #Combine de text and save as a Text File
-with Path('pdfToText.txt').open(mode='w') as output_file:
-    text = ''
-    for page in pdf.pages:
-        text += page.extractText()
-        output_file.write(text)
+# with Path('pdfToText.txt').open(mode='w') as output_file:
+#     text = ''
+#     for page in pdf.pages:
+#         text += page.extractText()
+#         output_file.write(text)
 
-        #Where is the word
+#         #Where is the word
 
-        word_pages = []
-        Word = 'DOMICILIACIÓN'
-        for page in pdf.pages:
-            page_num = page['/StructParents']
-            page_text = page.extractText()
+#         word_pages = []
+#         Word = 'DOMICILIACIÓN'
+#         for page in pdf.pages:
+#             page_num = page['/StructParents']
+#             page_text = page.extractText()
 
-            if Word in page_text:
-                word_pages.append(page_num)
+#             if Word in page_text:
+#                 word_pages.append(page_num)
 
 
-print(word_pages)
+# print(word_pages)
 
+reg: '/\b(Domiciliacion)\b/i'
+
+last_row_number = sheet1.max_row
+print(last_row_number)
+sheet1.cell(column=1,row=last_row_number + 1) == domiciliacion
+
+excel.save('excelprueba.xlsx')
---
+++ prueba.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[300]:
+
+
+import re
+import pdfplumber
+import pandas as pd
+from collections import namedtuple
+
+
+# In[301]:
+
+
+tipo10PorCiento = namedtuple('Line', 'Base')
+
+
+# In[302]:
+
+
+#registro = re.compile()
+
+
+# In[303]:
+
+
+tipo10PorCiento(* '1 539.49 878121.61 1523.96 - -44504.79 1 1 -'.split())
+
+
+# In[304]:
+
+
+file = './303 2019 07.pdf'
+monthRegex = re.compile(r"(0?[1-9]|[1][0-2])$")
+
+
+# In[305]:
+
+
+lines = []
+total_check = 0
+
+with pdfplumber.open(file) as pdf:
+    pages = pdf.pages
+    for page in pdf.pages:
+        text = page.extract_text()
+        for line in text.split('\n') :
+            if(line.find('Ejercicio ') != -1):
+                month = monthRegex.search(line).group(1)
+                print(month) 
+            elif line.startswith('Régimen general'):
+                 
+                if(line.startswith('04')  != -1):
+                    items = line.split()
+                    print(line)   
+                    items = line.split()
+           # print(items)   
+           # lines.append(Line(*items))           
+           # total_check += tot
+
+
+# In[293]:
+
+
+lines[:1]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
